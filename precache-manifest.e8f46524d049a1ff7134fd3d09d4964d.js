d515eb483451a06d7d5b8d8101498100522955ef
akkewacheiei <akkewach.yodsomboon@gmail.comm>
2020-07-27 00:52:24 +0700

    Updates

---
+++ precache-manifest.e8f46524d049a1ff7134fd3d09d4964d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3c17c42e689c307a75b6beb0f289da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68bec36bf29ba9de3984",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "92b6fa60c892b4669542",
+    "url": "/static/css/main.d07f8403.chunk.css"
+  },
+  {
+    "revision": "68bec36bf29ba9de3984",
+    "url": "/static/js/2.e57799b0.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.e57799b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b6fa60c892b4669542",
+    "url": "/static/js/main.6c7f6cf7.chunk.js"
+  },
+  {
+    "revision": "5eeb8d0fd802ead6c0b5",
+    "url": "/static/js/runtime-main.aa4df6ff.js"
+  }
+]);
